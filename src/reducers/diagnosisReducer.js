b0f0734f655700ec983164e407e832c4d8e43d6a
anatol-blackforest <anatolblackforest@gmail.com>
2017-10-31 16:34:55 +0200

    Switch

--- src/reducers/diagnosisReducer.js
+++ src/reducers/diagnosisReducer.js
@@ -11,8 +11,10 @@ let initialState = {
 }
 
 export default function articlesReducer(state = initialState, action){
-  //поиск диагноза в массиве
-  if(action.type === "SEARCH_DIAGNOSIS"){
+  switch(action.type){
+
+    //поиск диагноза в массиве
+    case "SEARCH_DIAGNOSIS": {
       if(action.payload.length){
           const newArray = state.diagnoses.filter((item, index) => {
               return (item.diagnosis.indexOf(action.payload) !== -1 || item.description.indexOf(action.payload) !== -1)
@@ -21,80 +23,91 @@ export default function articlesReducer(state = initialState, action){
             ...state,
             filteredDiagnoses: newArray
           }
-     }else{
+      }else{
         return{
             ...state,
             filteredDiagnoses: []
         }
-     }
-  }else if(action.type === "DISPLAY_DIAGNOSIS"){
+      }
+    }
+
     //отображение подробного диагноза в боковой панели
-     let diagnose;
-     state.diagnoses.map((item) => {
-       if (item.id === action.payload){
-           diagnose = item;
+    case "DISPLAY_DIAGNOSIS": {
+       let diagnose;
+       state.diagnoses.map((item) => {
+         if (item.id === action.payload){
+             diagnose = item;
+         }
+       });
+       return{
+          ...state,
+          currentDiagnose: {
+            id: action.payload,
+            diagnosis: diagnose.diagnosis,
+            description: diagnose.description
+          }
        }
-     });
-     return{
-        ...state,
-        currentDiagnose: {
-          id: action.payload,
-          diagnosis: diagnose.diagnosis,
-          description: diagnose.description
-        }
-     }
-  }else if(action.type === "DELETE_DIAGNOSIS"){
+    }
+
     //удаление диагноза
-     let newState = state.diagnoses.filter((item) => {
-       return item.id !== action.payload
-     });
-     if(state.currentDiagnose.id === action.payload){
-        return{
+    case "DELETE_DIAGNOSIS": {
+       let newState = state.diagnoses.filter((item) => {
+         return item.id !== action.payload
+       });
+       if(state.currentDiagnose.id === action.payload){
+          return{
+              ...state,
+              diagnoses: newState,
+              currentDiagnose: {
+                id: null,
+                diagnosis: "",
+                description: ""
+              }
+          }
+       }
+       return{
+          ...state,
+          diagnoses: newState
+       }
+    }
+
+    //редактирование диагноза в боковой панели
+    case "DELETE_DIAGNOSIS": {
+         let newState = [...state.diagnoses];
+         newState = newState.map((item, index) => {
+           let newItem = {...item}
+           if(item.id === Number(action.payload.id)){
+             newItem.description = action.payload.description;
+             return newItem;
+           }
+           return newItem
+         });
+         return{
             ...state,
             diagnoses: newState,
             currentDiagnose: {
-              id: null,
-              diagnosis: "",
-              description: ""
+              ...state.currentDiagnose,
+              description: action.payload.description
             }
-        }
-     }
-     return{
-        ...state,
-        diagnoses: newState
-     }
-  }else if(action.type === "EDIT_DIAGNOSIS"){
-    //редактирование диагноза в боковой панели
-     let newState = [...state.diagnoses];
-     newState = newState.map((item, index) => {
-       let newItem = {...item}
-       if(item.id === Number(action.payload.id)){
-         newItem.description = action.payload.description;
-         return newItem;
-       }
-       return newItem
-     });
-     return{
-        ...state,
-        diagnoses: newState,
-        currentDiagnose: {
-          ...state.currentDiagnose,
-          description: action.payload.description
-        }
-     }
-  }else if(action.type === "ADD_DIAGNOSIS"){
+         }
+    }
+
     //добавление нового диагноза
-     return{
-       ...state,
+    case "ADD_DIAGNOSIS": {
+      return{
+        ...state,
         diagnoses:[
-            ...state.diagnoses,
-            {
-                "id": Date.now(),
-                "diagnosis": action.payload.diagnosis,
-                "description": action.payload.description
-            }
+          ...state.diagnoses,
+          {
+              "id": Date.now(),
+              "diagnosis": action.payload.diagnosis,
+              "description": action.payload.description
+          }
         ]
-     }
+      }
+    }
+
+    default: return state
+    
   }
-  return state
 };
