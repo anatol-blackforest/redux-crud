d2997278b77d0cd9cef2f2f5ac2d5314da786bbc
anatol-blackforest <anatolblackforest@gmail.com>
2017-11-06 17:24:43 +0200

    openTrigger noname

--- src/reducers/openTriggerReducer.js
+++ src/reducers/openTriggerReducer.js
@@ -1,6 +1,6 @@
 const data = {prepareToEdit: false}
 //редюсер видимости формы редактирования
-export default function articlesReducer(state = data, action){
+export default function (state = data, action){
   if(action.type === "OPEN_TRIGGER"){
      return  {
         prepareToEdit: !state.prepareToEdit
